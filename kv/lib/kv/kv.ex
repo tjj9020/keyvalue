952b7d8f2155887d7244d78268b60f35ba3f2939
Tim Johnson <timothy.johnson2@purina.nestle.com>
2019-04-21 11:22:37 -0400

    add missing end

--- kv/lib/kv/kv.ex
+++ kv/lib/kv/kv.ex
@@ -14,6 +14,6 @@ defmodule KV.Bucket do
     end
 
     def delete(bucket, key) do
-        Agent.get_and_update(bucket, fn dict -> Map.pop(dict, key))
+        Agent.get_and_update(bucket, fn dict -> Map.pop(dict, key) end)
     end
 end

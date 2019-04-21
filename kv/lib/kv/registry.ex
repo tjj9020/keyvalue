0bfe759d112acb93243c94ac597b54dfea65969c
Tim Johnson <timothy.johnson2@purina.nestle.com>
2019-04-21 11:22:31 -0400

    add missing end

--- kv/lib/kv/registry.ex
+++ kv/lib/kv/registry.ex
@@ -20,4 +20,17 @@ defmodule KV.Registry do
   def init(:ok) do
     {:ok, %{}}
   end
+
+  def handle_call({:lookup, name}, _from, names) do
+    {:reply, Map.fetch(names, name), names}
+  end
+
+  def handle_cast({:create, name}, names) do
+    if Map.has_key?(names, name) do
+      {:noreply, names}
+    else
+      {:ok, bucket} = KV.Bucket.start_link([])
+      {:noreply, Map.put(names, name, bucket)}
+    end
+  end
 end
